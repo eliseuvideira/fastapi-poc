3651053904e6fafa1b8643700472b8ebc005ea33
Eliseu Videira <eliseuvideira1@gmail.com>
2022-08-30 08:40:13 -0300

    :sparkles: added credentials + environment variables

--- app/main.py
+++ app/main.py
@@ -1,12 +1,19 @@
-from fastapi import FastAPI, File, UploadFile
-
+from dotenv import load_dotenv
+from fastapi import Depends, FastAPI, File, HTTPException, status, UploadFile
+from fastapi.security import HTTPBasic, HTTPBasicCredentials
 from deepface import DeepFace
-
 from uuid import uuid4
+from os import environ
 
+load_dotenv()
 
 app = FastAPI()
 
+security = HTTPBasic()
+
+PYTHON_ENV = environ.get("PYTHON_ENV", "production")
+API_BASIC_AUTH_USERNAME = environ.get("API_BASIC_AUTH_USERNAME")
+API_BASIC_AUTH_PASSWORD = environ.get("API_BASIC_AUTH_PASSWORD")
 
 async def save(image: UploadFile):
     file_location = f"/tmp/{uuid4()}"
@@ -16,7 +23,12 @@ async def save(image: UploadFile):
 
 
 @app.post("/")
-async def compare_images(current: UploadFile, original: UploadFile):
+async def compare_images(current: UploadFile, original: UploadFile, credentials: HTTPBasicCredentials = Depends(security)):
+    if (credentials.username != API_BASIC_AUTH_USERNAME or credentials.password != API_BASIC_AUTH_PASSWORD):
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+        )
+
     current_filepath = await save(current)
     original_filepath = await save(original)
 
